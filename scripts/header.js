d16afc770a5676d75242b35628dc6e4eb8925c7a
sungeun <cmqktk18@gmail.com>
2025-07-08 21:29:46 +0900

    2025/07/08

--- scripts/header.js
+++ scripts/header.js
@@ -2,5 +2,5 @@ const swiper = new Swiper('.slide',{
     //옵션:값,
     autoplay:{delay:7000},
     loop:true,
-    direction:'horizontal', // horizontal, vertica
+    direction:'horizontal', //아래에서 위로 올라오게 horizontal, vertical
 })
