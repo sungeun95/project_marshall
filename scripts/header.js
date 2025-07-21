5b7a19ac55093977c3050c26360a661d56e4d7d6
sungeun <cmqktk18@gmail.com>
2025-07-21 15:13:58 +0900

    수정

--- scripts/header.js
+++ scripts/header.js
@@ -3,6 +3,9 @@ const swiper = new Swiper('.slide',{
     autoplay:{delay:7000},
     loop:true,
     direction:'horizontal', //아래에서 위로 올라오게 horizontal, vertical
+    pagination: {
+        el: ".swiper-pagination",
+    },
 })
 
 /* const aAll = document.querySelectorAll('a');
